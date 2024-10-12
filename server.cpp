dffcb3c7f0f640781fa103fa0bf7277baa10c173
Unnar G <unnar@outlook.com>
2024-10-12 20:05:42 +0000

    first draft main

--- server.cpp
+++ server.cpp
@@ -25,7 +25,6 @@
 #include "poll-manager.hpp"
 #include "group-message-manager.hpp"
 
-#define MAX_EOT_TRIES 5
 
 // fix SOCK_NONBLOCK for OSX
 #ifndef SOCK_NONBLOCK
@@ -33,6 +32,7 @@
 #define SOCK_NONBLOCK O_NONBLOCK
 #endif
 
+#define MAX_EOT_TRIES 5
 #define Backlog 5
 #define MAX_MESSAGE_LENGTH 3*5000
 
@@ -158,6 +158,80 @@ void sendKeepAliveMessages(ServerManager &serverManager)
     }
 }
 
+void handleNewConnection(int &listenSock, char *serverPort, char *GROUP_ID)
+{
+    int clientSock;
+    struct sockaddr_in client;
+    socklen_t clientLen = sizeof(client);
+
+    // New connection on the listening socket
+    clientSock = accept(listenSock, (struct sockaddr *)&client, &clientLen);
+
+    char clientIpAddress[INET_ADDRSTRLEN]; // Buffer to store the IP address
+    inet_ntop(AF_INET, &(client.sin_addr), clientIpAddress, INET_ADDRSTRLEN);
+
+    serverManager.add(clientSock, clientIpAddress);
+    serverManager.update(clientSock, serverPort);
+    pollManager.add(clientSock);
+
+    logger.write("New client connected: " + std::string(clientIpAddress), true);
+    std::string serverMessage = constructServerMessage("HELO," + std::string(GROUP_ID));
+    send(clientSock, serverMessage.c_str(), serverMessage.length(), 0);
+    
+}
+
+// returns -1 = error, 0 = client disconnected, 1 = message received, 2 = message dropped
+int recvAndParseMsg(char *buffer, int &clientSocket, std::string &serverName){
+    // Read the data into a buffer, it's expected to start with SOH and and with EOT
+    // however, it may be split into multiple packets, so we need to handle that.
+    for (int i = 0, offset = 0, bytesRead = 0; i < MAX_EOT_TRIES; i++, offset += bytesRead)
+    {
+        bytesRead = recv(clientSocket, buffer + offset, sizeof(buffer) - offset, 0);
+
+        if (bytesRead == -1)
+        {
+            logger.write("ERROR: Failed to read from " + serverName + ", received -1 from recv. Error: " + strerror(errno), true);
+            return -1;
+        }
+
+        logger.write("Received from " + serverName, buffer + offset, bytesRead);
+
+        if (offset + bytesRead >= MAX_MESSAGE_LENGTH)
+        {
+            logger.write("Message from " + serverName + ": message exceeds " + std::to_string(MAX_MESSAGE_LENGTH) + " bytes.", true);
+            return 2;
+        }
+
+        if (buffer[0] != SOH && offset == 0)
+        {
+            logger.write("Invalid message format (First packet did not start with SOH) from " + serverName, buffer, bytesRead, true);
+            return -1;
+        }
+
+        if (bytesRead == 0)
+        {
+            logger.write("Remote server disconnected: " + serverName, true);
+            return 0;
+        }
+
+        if (buffer[offset + bytesRead - 1] == EOT && buffer[offset + bytesRead - 2] != ESC)
+        {
+            buffer[offset + bytesRead] = '\0';
+            return 1;
+        }
+
+        if (i == MAX_EOT_TRIES - 1)
+        {
+            // Tried to read the message MAX_EOT_TRIES times, but still no EOT
+            logger.write("Invalid message format (No SOH, EOT) from " + serverName, buffer, bytesRead + offset, true);
+            return -1;
+        }
+    }
+
+    logger.write("recvAndParseMsg: Should not reach here", true);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     char *serverPort = argv[1];
@@ -168,10 +242,7 @@ int main(int argc, char *argv[])
     clientCommands.setGroupId(GROUP_ID);
     serverCommands.setPort(serverPort);
 
-    int listenSock, clientSock;
-    struct sockaddr_in client;
-    socklen_t clientLen = sizeof(client);
-    char buffer[MAX_MESSAGE_LENGTH]; // Buffer for reading from clients
+    int listenSock;
 
     if (argc != 3)
     {
@@ -196,12 +267,11 @@ int main(int argc, char *argv[])
     // Main server loop
     while (true)
     {
-
         int pollCount = pollManager.getPollCount();
 
         if (pollCount == -1)
         {
-            logger.write("Poll failed", true);
+            logger.write("Poll failed: " + std::string(strerror(errno)), true);
             close(listenSock);
             break;
         }
@@ -209,22 +279,10 @@ int main(int argc, char *argv[])
         // Check for events on the listening socket
         if (pollManager.hasData(0)) // listen sock should always be 0
         {
-            // New connection on the listening socket
-            clientSock = accept(listenSock, (struct sockaddr *)&client, &clientLen);
-
-            char clientIpAddress[INET_ADDRSTRLEN]; // Buffer to store the IP address
-            inet_ntop(AF_INET, &(client.sin_addr), clientIpAddress, INET_ADDRSTRLEN);
-
-            serverManager.add(clientSock, clientIpAddress);
-            serverManager.update(clientSock, serverPort);
-            pollManager.add(clientSock);
-
-            logger.write("New client connected: " + std::string(clientIpAddress), true);
-            std::string serverMessage = constructServerMessage("HELO," + std::string(GROUP_ID));
-            send(clientSock, serverMessage.c_str(), serverMessage.length(), 0);
+            handleNewConnection(listenSock, serverPort, GROUP_ID);
         }
 
-        // Check for events on existing connections (clients)
+        // Check for events on the remote-server sockets
         for (int i = 1; i < pollManager.nfds; i++)
         {
             if (!pollManager.hasData(i))
@@ -232,61 +290,23 @@ int main(int argc, char *argv[])
                 continue;
             }
 
+            char buffer[MAX_MESSAGE_LENGTH]; // Buffer for reading from clients
             int clientSocket = pollManager.getFd(i);
             std::string serverName = serverManager.getName(clientSocket);
 
-            // Read the data into a buffer, it's expected to start with SOH and and with EOT
-            // however, it may be split into multiple packets, so we need to handle that.
-            // We also need to handle the cases where the client does not wrap the message in SOH and EOT
-            for (int i = 0, offset = 0, bytesRead = 0; i < MAX_EOT_TRIES; i++, offset += bytesRead)
+            int recvResult = recvAndParseMsg(buffer, clientSocket, serverName);
+            if(recvResult == -1 || recvResult == 0)
             {
-                bytesRead = recv(clientSocket, buffer + offset, sizeof(buffer) - offset, 0);
-
-                if (bytesRead == -1)
-                {
-                    std::string errorMsg = "ERROR: Failed to read from " + serverName + ", received -1 from recv. Error: " + strerror(errno);
-                    logger.write(errorMsg, true);
-                    closeClient(clientSocket);
-                    break;
-                }
-
-                logger.write("Received from " + serverName, buffer + offset, bytesRead);
-
-                if (offset + bytesRead >= MAX_MESSAGE_LENGTH)
-                {
-                    logger.write("Dropped message from " + serverName + ": message exceeds " + std::to_string(MAX_MESSAGE_LENGTH) + " bytes.", true);
-                    break;
-                }
-
-                if (buffer[0] != SOH && offset == 0)
-                {
-                    logger.write("Invalid message format (First packet did not start with SOH) from " + serverName, buffer, bytesRead, true);
-                    closeClient(clientSocket);
-                    break;
-                }
-
-                if (bytesRead <= 0)
-                {
-                    logger.write("Remote server disconnected: " + serverName, true);
-                    closeClient(clientSocket);
-                    break;
-                }
-
-                if (buffer[offset + bytesRead - 1] == EOT && buffer[offset + bytesRead - 2] != ESC)
-                {
-                    buffer[offset + bytesRead] = '\0';
-                    handleCommand(clientSocket, buffer);
-                    break;
-                }
-
-                if (i == MAX_EOT_TRIES - 1)
-                {
-                    // Tried to read the message MAX_EOT_TRIES times, but still no EOT
-                    logger.write("Invalid message format (No SOH, EOT) from " + serverName, buffer, bytesRead + offset, true);
-                    closeClient(clientSocket);
-                    break;
-                }
+                closeClient(clientSocket);
+                logger.write("Closing connection to " + serverName, true);
+                continue;
+            } 
+        
+            if(recvResult == 1)
+            {
+                handleCommand(clientSocket, buffer);
             }
+
         }
     }
 
